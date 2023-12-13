157cf168586a59c00d348b5bddedc6c8f97fbabf
lauryngoodwork <lauryn@goodwork.sg>
2023-12-13 23:39:44 +0800

    feat: index page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,57 +1,101 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import classNames from "classnames";
+import Link from "next/link";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Page(): JSX.Element {
-  const [scrollY, setScrollY] = useState<number>(0);
-  const height = 8000;
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollY(window.scrollY);
-      console.log("sup", window.scrollY);
-    };
-    handleScroll();
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <main className="relative w-full block overflow-auto">
-      <div
-        className={"w-full top-0 overflow-auto left-0 bg-purple-100"}
-        style={{
-          height: `${height}px`,
-        }}
-      >
-        <div className="w-screen bg-blue-500/50 block h-screen fixed bottom-[20px] pointer-events-none">
-          <div
-            className="absolute p-6 bg-red-300 text-black pointer-events-auto"
-            style={{
-              top: "50%",
-              left: `${(scrollY / height) * 50}%`,
-              transform: "translate(0, -50%)",
-            }}
-          >
-            hello
-          </div>
-          <div
-            className="absolute p-6 bg-green-300 text-black pointer-events-auto"
-            style={{ top: "50%", right: "0%", transform: "translate(0, -50%)" }}
-          >
-            world
+    <main className="p-4 w-full flex">
+      <div className="w-[350px] h-[450px] flex-shrink-0 border-t border-r transition-all hover:rounded-full hover:px-12 hover:py-12 rounded-se-full border-white pl-4 pr-20 py-0 hover:bg-white hover:text-black">
+        <div className="border-b w-full border-white pb-4">
+          <h1 className="font-mono text-2xl ">Escape Theme Park</h1>
+          <p className="font-mono text-md">
+            By{" "}
+            <a
+              className="hover:bg-black hover:text-white"
+              target="_blank"
+              href="https://instagram.com/bananaleow"
+            >
+              Lauryn Leow
+            </a>{" "}
+            and{" "}
+            <a
+              target="_blank"
+              className="hover:bg-black hover:text-white"
+              href="https://instagram.com/isachanjh"
+            >
+              Isabella Chan
+            </a>
+          </p>
+        </div>
+        <div className="py-4 font-mono">
+          <p>
+            Hello, welcome to Escape Theme Park! This is an experimental space
+            created for us to just mess around. This space is still a wip.
+            Please bear with us. Have fun looking!
+          </p>
+          <div className=" mt-4">
+            <a
+              className="hover:bg-black hover:text-white"
+              target="_blank"
+              href="https://github.com/laurndrytime/escape-theme-park"
+            >
+              Github
+            </a>
           </div>
         </div>
       </div>
-      <section className="bg-yellow-500 w-full h-screen">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe fugit
-        provident at assumenda nobis ipsam facere, accusantium quisquam esse.
-        Earum quia neque assumenda officia architecto eos facilis laborum
-        dignissimos tempora.
-      </section>
+      <div className="w-full">
+        <div className="h-[450px] px-4">
+          <h3 className="font-mono mb-4 text-xl">Index</h3>
+          <Toggle label={"Scrollers"}>
+            <Link href="/scroll/translate">Scroll Translation</Link>
+            <Link href="/scroll/snap">Scroll Snap</Link>
+          </Toggle>
+        </div>
+        <div className="h-[300px] -ml-[1px] border-b border-l rounded-es-full w-full"></div>
+      </div>
     </main>
+  );
+}
+
+function Toggle({
+  label,
+  children,
+}: {
+  label: string;
+  children: React.ReactNode;
+}) {
+  const [expand, setExpand] = useState<boolean>(true);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setExpand((expand) => !expand);
+        }}
+        className=" border-b border-white font-mont text-lg"
+      >
+        {label}{" "}
+        <span
+          className={classNames("transition-all", expand ? "" : "rotate-180")}
+        >
+          ↯
+        </span>
+      </button>
+      <AnimatePresence>
+        {expand && (
+          <motion.div
+            className="flex flex-col gap-2 py-4"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+          >
+            {children}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 }

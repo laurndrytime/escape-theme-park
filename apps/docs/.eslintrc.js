4e1e9b92132e14b4fed6d84353b34bba643cda7d
lauryngoodwork <lauryn@goodwork.sg>
2023-12-24 15:28:16 +0800

    fix: removed docs

--- apps/docs/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@escape-theme-park/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {},
-};
